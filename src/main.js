bb4e6e56a8025aeaf53ed7a1a2ac90dc23aa2c98
Line <line@datahjelpen.no>
2020-01-21 09:56:11 +0100

    Adding vuex store to the app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  el: '#app',
+  store,
+  components: { App },
+  template: '<App/>',
+  render: h => h(App)
 }).$mount('#app')
